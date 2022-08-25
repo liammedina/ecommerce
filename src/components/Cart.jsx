d1ab62a41972b541d7fc3e23616c7da1f72356c2
Lia M. Medina <54250767+Niugler@users.noreply.github.com>
2022-08-25 19:13:08 -0300

    depurando

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../Context/CartContext";
 import ItemCart from "./ItemCart";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 import {addDoc, collection, getFirestore} from "firebase/firestore"
 import '../styles/Cart.css'
 
@@ -18,15 +20,17 @@ const Cart = () => {
             address: 'zaraza 1235'
         },
         items: cart.map(product => ({id: product.id, title: product.title, price: product.price, quantity: product.quantity })),
-        total: totalPrice,
+        total: totalPrice(),
     }
     const handleClick = () => {
         const db = getFirestore();
         const ordersCollection = collection(db, 'orders');
         addDoc(ordersCollection, order)
-        .then(({ id }) => console.log(id))
+        .then(({ id }) => Swal.fire( 'Te compra ha sido realizada con éxito!',
+        'El N* de tu órden es '+ id,
+        'success') )
+        
     }
-
     if (cart.length === 0) {
         return(
             <div className="emplyCart">
@@ -35,25 +39,28 @@ const Cart = () => {
             </div>
         )
     }
-    
-        return ( 
+    const mostarAlert = () =>{
+        Swal.fire( 'Te compra ha sido realizada con éxito!',
+        'El N* de tu órden es ...' ,
+        'success');
+    } 
+    return ( 
             <>
             {cart.map(product => <ItemCart key={product.id} product={product} />)} 
             <h2 className="total" >Total: $ {totalPrice()}</h2>
-            
             <form  className="form">
-                      <h2>Ingrese sus Datos:</h2>
-                      <label htmlFor="name" className="renglon">Nombre Completo</label>
-                      <input type="text" name="name" id="name" placeholder="Nombre y Apellido" className="renglon"/>
+                <h2>Ingrese sus Datos:</h2>
+                <label htmlFor="name" className="renglon">Nombre Completo</label>
+                <input type="text" name="name" id="name" placeholder="Nombre y Apellido" className="renglon"/>
 
-                      <label htmlFor="tel" className="renglon" > Telefono </label>
-                      <input type="tel" name="tel" id="tel" placeholder="11-xxxx-xxxx" className="renglon" />
+                <label htmlFor="tel" className="renglon" > Telefono </label>
+                <input type="tel" name="tel" id="tel" placeholder="11-xxxx-xxxx" className="renglon" />
 
-                      <label htmlFor="email" className="renglon">Email</label>
-                      <input type="email" name="email" id="email"vplaceholder="you@example.com" className="renglon" />
-
-                      <button type="submit" onClick={handleClick} className="btn btn-outline-success" > Finalizar Compra </button>
-                    </form>
+                <label htmlFor="email" className="renglon">Email</label>
+                <input type="email" name="email" id="email"vplaceholder="you@example.com" className="renglon" />
+            </form>
+            <button onClick={handleClick} className="btn btn-outline-success" > Finalizar Compra </button>
+            {}
             </>
          );
     }
